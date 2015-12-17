eda9ca415e5aa536429875cf680ad526dbf2af28
Mark Tabler <mtabler@newrelic.com>
2015-12-16 22:43:32 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zoogenius::Application.config.secret_key_base = 'e16e8b1b0e41406385c940f774bfd2727c01333cddf01a4ed049db1b94b73afbb4ed1ae49e9cd387470506012cf7130728b262843a6591619c24bbd7857fa6d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zoogenius::Application.config.session_store :cookie_store, key: '_zoogenius_session'
